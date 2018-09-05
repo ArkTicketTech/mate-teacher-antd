f1f2461edb2a23c359cdcb6695db79dfea65dd03
hd00300 <hd00300@sjtu.edu.cn>
2018-09-05 12:06:46 +0800

    update

--- src/components/EditableTable.js
+++ src/components/EditableTable.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table, Divider, Button, Input, InputNumber, Form, Popconfirm } from 'antd';
+import { Table, Input, Button, Popconfirm, Form } from 'antd';
 
 const FormItem = Form.Item;
 const EditableContext = React.createContext();
@@ -103,119 +103,85 @@ class EditableCell extends React.Component {
     }
 }
 
-class CoursesList extends React.Component {
+class EditableTable extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            testArray: [1, 2],
-            editingKey: '',
-            selectedRowKeys: [],
-            courses: [],
-            count: 15,
-        };
-
         this.columns = [{
-            title: '课程名称',
+            title: 'name',
             dataIndex: 'name',
+            width: '30%',
             editable: true,
-            key: 'name'
         }, {
-            title: '上课时间',
-            dataIndex: 'season',
+            title: 'age',
+            dataIndex: 'age',
             editable: true,
-            key: 'season'
         }, {
-            title: '上课人数',
-            dataIndex: 'members',
+            title: 'address',
+            dataIndex: 'address',
             editable: true,
-            key: 'members'
         }, {
-            title: '上课地点',
-            dataIndex: 'site',
-            editable: true,
-            key: 'site'
-        }, {
-            title: '操作',
-            key: 'action',
-            render: () => (
-                <span>
-                    <a href="javascript::">问卷状态</a>
-                    <Divider type="vertical" />
-                    <a href="javascript::">生成报告</a>
-                </span>
-            )
-        }];
-
-        for (let i = 0; i < 15; i++) {
-            this.state.courses.push({
-                key: i,
-                name: '示例课程'+i,
-                season: '2018秋季',
-                members: 30 + i,
-                site: '东上院507',
-                removed: false,
-            });
-        }
+            title: 'operation',
+            dataIndex: 'operation',
+            render: (text, record) => {
+                return (
+                    this.state.dataSource.length >= 1 ? (
+                        <Popconfirm title="Sure to delete?" onConfirm={() => this.handleDelete(record.key)}>
+                            <a href="javascript::">Delete</a>
+                        </Popconfirm>
+                    ) : null
+                );
+            }
+            }];
+        
+        this.state = {
+            dataSource: [{
+                key: '0',
+                name: 'Edward King 0',
+                age: '32',
+                address: 'London, Park Lane no.0',
+            }, {
+                key: '1',
+                name: 'Edward King 1',
+                age: '32',
+                address: 'London, Park Lane no.1',
+            }],
+            count: 2,
+        };
     }
 
-    onSelectChange = (selectedRowKeys) => {
-        console.log('seletedRowKeys changed: ', selectedRowKeys);
-        // console.log('test array output: ', this.state.testArray);
-        // var index;
-        // for (index in this.state.testArray) {
-            // console.log(Number(index), typeof Number(index));
-        // }
-        this.setState({ selectedRowKeys });
+    handleDelete = (key) => {
+        console.log(key, typeof key);
+        const dataSource = [...this.state.dataSource];
+        this.setState({ dataSource: dataSource.filter(item => item.key !== key) });
     }
 
-    remove = () => {
-        const newCourses = [...this.state.courses];
-        var newSelectedRowKeys = this.state.selectedRowKeys;
-        console.log('seletedRowKeys changed: ', newSelectedRowKeys);
-        console.log('Courses changed: ', newCourses);
-        var i = newSelectedRowKeys.length;
-        while (i--) {
-            console.log(typeof newSelectedRowKeys[i]);
-            newCourses[newSelectedRowKeys[i]].removed = true;
-            // newCourses.filter(item => item.key !== newSelectedRowKeys[i]);
-        }
-        this.setState({ selectedRowKeys: [], Courses: newCourses });
+    handleAdd = () => {
+        const { count, dataSource } = this.state;
+        const newData = {
+            key: count,
+            name: 'Edward King ' + count,
+            age: 32,
+            address: 'London, Park Lane no.' + count,
+        };
+        this.setState({
+            dataSource: [newData, ...dataSource],
+            count: count + 1,
+        });
     }
 
     handleSave = (row) => {
-        const newData = [...this.state.courses];
+        const newData = [...this.state.dataSource];
         const index = newData.findIndex(item => row.key === item.key);
         const item = newData[index];
         newData.splice(index, 1, {
             ...item,
             ...row,
         });
-        this.setState({ courses: newData });
-    }
-
-    handleAdd = () => {
-        const { count, courses } = this.state;
-        const newData = {
-            key: count,
-            name: '示例课程' + count,
-            season: '2018秋季',
-            members: 30 + count,
-            site: '东上院507',
-            removed: false,
-        };
-        this.setState({
-            courses: [newData, ...courses],
-            count: count + 1,
-        });
+        this.setState({ dataSource: newData });
     }
 
     render() {
-        const { selectedRowKeys, courses } = this.state;
-        const rowSelection = {
-            selectedRowKeys,
-            onChange: this.onSelectChange,
-        };
-        const hasSelected = selectedRowKeys.length > 0;
+        const { dataSource } = this.state;
         const components = {
             body: {
                 row: EditableFormRow,
@@ -237,32 +203,21 @@ class CoursesList extends React.Component {
                 }),
             };
         });
-
         return (
             <div>
-                <span>
-                    <Button type="primary"
-                        style={{ margin: '5px 5px'}}
-                        onClick={this.handleAdd}>
-                        添加课程
-                    </Button>
-                    <Button type="primary"
-                        style={{ margin: '5px 5px' }}
-                        disabled={!hasSelected}
-                        onClick={this.remove}>
-                        移除课程
-                    </Button>
-                </span>
+                <Button onClick={this.handleAdd} type="primary" style={{ marginBottom: 16 }}>
+                    Add a row
+                </Button>
                 <Table
                     components={components}
                     rowClassName={() => 'editable-row'}
                     bordered
-                    rowSelection={rowSelection}
-                    dataSource={courses}
-                    columns={columns} />
+                    dataSource={dataSource}
+                    columns={columns}
+                />
             </div>
-        )
+        );
     }
 }
 
-export default CoursesList;
+export default EditableTable;
