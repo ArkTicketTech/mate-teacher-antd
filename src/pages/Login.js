665c40011a5affed3b13005a186759ebd3b85b10
hd00300 <hd00300@sjtu.edu.cn>
2018-10-12 16:16:12 +0800

    注册表单

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from '../logo.png';
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import { Form, Icon, Input, Button, Checkbox, Modal } from 'antd';
 
 const FormItem = Form.Item;
 
 class LoginForm extends React.Component {
-    handleSubmit = (e) => {
+    state = {
+        loading: false,
+        visible: false,
+        registering: false,
+    }
+
+    onLogin = (e) => {
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
             if (!err) {
@@ -15,8 +21,63 @@ class LoginForm extends React.Component {
         });
     }
 
+    onRegister = () => {
+        this.setState({
+            visible: true,
+            registering: true,
+        });
+    }
+
+    handleOk = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+                if (values.password !== values.confirm) {
+                    console.log('warning', values.password, values.confirm);
+                }
+            }
+        });
+        this.setState({ loading: true, registering: false });
+        setTimeout(() => {
+            this.setState({ loading: false, visible: false });
+        }, 3000);
+    }
+
+    handleCancel = () => {
+        this.setState({ visible: false });
+    }
+
+    compareToFirstPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && value !== form.getFieldValue('password')) {
+            callback('Two passwords that you enter is inconsistent!');
+        } else {
+            callback();
+        }
+    }
+
+    validateToNextPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && this.state.confirmDirty) {
+            form.validateFields(['confirm'], { force: true });
+        }
+        callback();
+    }
+
     render() {
         const { getFieldDecorator } = this.props.form;
+        const { visible, loading, registering } = this.state;
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 16 },
+                sm: { span: 8 },
+            },
+            wrapperCol: {
+                xs: { span: 16 },
+                sm: { span: 16 },
+            },
+        };
         return (
             <div>
                 <div className="App">
@@ -25,15 +86,19 @@ class LoginForm extends React.Component {
                     </header>
                     <h1 className="App-title">Welcome to Mate</h1>
                 </div>
-                <Form onSubmit={this.handleSubmit} className="login-form">
-                    <FormItem>
+                <Form onSubmit={this.onLogin} className="login-form">
+                    <FormItem
+                        {...formItemLayout}
+                        label="Username">
                         {getFieldDecorator('userName', {
                             rules: [{ required: true, message: 'Please input your username!' }],
                         })(
                             <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,..25)' }} />} placeholder="Username" />
                         )}
                     </FormItem>
-                    <FormItem>
+                    <FormItem
+                        {...formItemLayout}
+                        label="Password">
                         {getFieldDecorator('password', {
                             rules: [{ required: true, message: 'Please input your Password!' }],
                         })(
@@ -49,11 +114,74 @@ class LoginForm extends React.Component {
                         )}
                         <a className="login-form-forgot" href="">Forgot password</a>
                         <Button type="primary" htmlType="submit" className="login-form-button">
-                            Log in
+                            Login
                         </Button>
-                        Or <a href="">register now!</a>
+                        Or <a onClick={this.onRegister}>register now!</a>
                     </FormItem>
                 </Form>
+                <Modal
+                    visible={visible}
+                    title="Register"
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                    footer={null}
+                >
+                    <Form onSubmit={this.handleOk} >
+                        <FormItem
+                            {...formItemLayout}
+                            label="E-mail">
+                            {getFieldDecorator('Email', {
+                                rules: [{
+                                    required: registering, message: 'Please input your email address!'
+                                }, {
+                                    type: 'email', message: 'The input is not valid E-mail',
+                                }],
+                            })(
+                                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,..25)' }} />} placeholder="Email address" />
+                            )}
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="Username">
+                            {getFieldDecorator('userName', {
+                                rules: [{ required: registering, message: 'Please input your username!' }],
+                            })(
+                                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,..25)' }} />} placeholder="Username" />
+                            )}
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="Password">
+                            {getFieldDecorator('password', {
+                                rules: [{
+                                    required: registering, message: 'Please input your Password!'
+                                }, {
+                                    validator: this.validateToNextPassword,
+                                }],
+                            })(
+                                <Input prefix={<Icon type="password" style={{ color: 'rgba(0,0,0,..25)' }} />} type="password" placeholder="Password" />
+                            )}
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="Confirm">
+                            {getFieldDecorator('confirm', {
+                                rules: [{
+                                    required: registering, message: 'Please input your Password again!'
+                                }, {
+                                    validator: this.compareToFirstPassword,
+                                }],
+                            })(
+                                <Input prefix={<Icon type="password" style={{ color: 'rgba(0,0,0,..25)' }} />} type="password" placeholder="Confirm your password" />
+                            )}
+                        </FormItem>
+                        <FormItem>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                Register
+                            </Button>
+                        </FormItem>
+                    </Form>
+                </Modal>
             </div>
         )
     }
